8bb88ac402a937b852f9eced9854e2f960949d67
Waphyoo <110945410+Waphyoo@users.noreply.github.com>
2023-03-03 15:08:44 +0700

    Update average.cpp
    ok

--- average.cpp
+++ average.cpp
@@ -1,1 +1,22 @@
+#include<iostream>
 
+using namespace std;
+
+
+int main(int argc,char *argv[]){
+
+double sum=0;
+int count = argc-1;
+
+if(argc==1) cout<<"Please input numbers to find average.";
+else{
+for(int i=0;i<argc;i++){
+    sum+=atoi(argv[i]);
+}
+sum=sum/count;
+cout<<"---------------------------------"<<"\n";
+cout<<"Average of "<<count<<" numbers = "<<sum<<"\n";
+cout<<"---------------------------------";
+}
+return 0;
+}
